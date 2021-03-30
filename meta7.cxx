7b39bb4749411a274d04d6c8a2bdacdf8f487281
Sean Baxter <lightborn@gmail.com>
2021-03-29 22:46:59 -0400

    Added meta1-6

--- meta7.cxx
+++ meta7.cxx
@@ -15,11 +15,13 @@ inline int capture_call(const char* cmd, char* text, int len) {
 void print_version() {
   // Make a format specifier to print the first 10 digits of the git hash.
   @meta const char* fmt =
+    "--version:\n"
     "  Circle compiler\n"
     "  Written by Sean Baxter\n"
     "  Compiled %s"
     "  Build %d\n"
-    "  Hash: %.10s\n";
+    "  Hash %.10s\n"
+    "  Machine %s\n";
 
   // Get the time of compilation.
   @meta time_t t = time(0);
@@ -28,11 +30,14 @@ void print_version() {
   // Retrieve the current commit hash. The hash is 40 digits long, and we
   // include space for null.
   @meta char hash[41];
-  @meta int len = capture_call("git rev-parse HEAD", hash, 41);
+  @meta capture_call("git rev-parse HEAD", hash, 41);
+
+  @meta char uname[100];
+  @meta capture_call("uname -srm", uname, 100);
 
   // Substitute into the format specifier.
-  @meta char text[120];
-  @meta sprintf(text, fmt, time, __circle_build__, hash);
+  @meta char text[200];
+  @meta sprintf(text, fmt, time, __circle_build__, hash, uname);
   
   // Convert text to a string literal, then print that.
   puts(@string(text));
